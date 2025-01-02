fda63ec0b909a627bdf2e0953ac3fc3b99ead5e1
Shashwat Dwivedi <117923310+Shashwatdwivedi30@users.noreply.github.com>
2025-01-02 23:26:08 +0530

    Time: 0 ms (100%), Space: 57.6 MB (18.27%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -8,7 +8,7 @@ reverse(nums,0,k-1);
 reverse(nums,k,n-1);
 }
 private void reverse(int[]nums,int start,int end){
-while(start<=end){
+while(start<end){
     int temp=nums[start];
     nums[start]=nums[end];
     nums[end]=temp;
