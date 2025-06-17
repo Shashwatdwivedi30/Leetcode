44d5082fc8547c0e11872082b93ac62fef9bf276
Shashwat Dwivedi <shashwatdwivedi3011@gmail.com>
2025-06-17 13:26:28 +0530

    Time: 3 ms (50.6%), Space: 46 MB (32.33%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int m = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(i <= m){
+                m = Math.max(m, i+nums[i]);
+            }
+        }
+        return m>=nums.length-1;
+    }
+}
