7ede5cc27c322bdbe717101bfc6636d9f75717d0
Shashwat Dwivedi <shashwatdwivedi3011@gmail.com>
2025-06-25 09:22:20 +0530

    Time: 57 ms (34.98%), Space: 45.9 MB (18.2%) - LeetHub

---
+++ 0729-my-calendar-i/0729-my-calendar-i.java
@@ -0,0 +1,24 @@
+class MyCalendar {
+
+    ArrayList<int[]> event;
+    public MyCalendar() {
+        event = new ArrayList<>();
+    }
+    
+    public boolean book(int startTime, int endTime) {
+        for(int[] curr : event){
+            int currstart = curr[0];
+            int currend = curr[1];
+
+            if(startTime < currend && currstart < endTime) return false;
+        }
+        event.add(new int[]{startTime, endTime});
+        return true;
+    }
+}
+
+/**
+ * Your MyCalendar object will be instantiated and called as such:
+ * MyCalendar obj = new MyCalendar();
+ * boolean param_1 = obj.book(startTime,endTime);
+ */
